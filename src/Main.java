b14d385060dfdabf256dd582464a5d5679db13a9
Dieggo <diego.fuentes@alu.uclm.es>
2023-03-09 10:57:26 +0100

    Creacion metodo hola

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
     }
+    public void Hola(){
+        System.out.println("hola");
+    }
 }
